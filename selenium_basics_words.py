b5e5dd96c01bb0aba28e84e75b0d7fb9f44ab0d0
jaybuck08 <felix.jr.njoku@gmail.com>
2024-11-02 14:20:23 +0000

    update

---
+++ selenium_basics_words.py
@@ -0,0 +1,14 @@
+
+
+# use this below to switch the driver to other sections like; alert, i-frame etc. this means that if "driver." is used, it will be searching for elements within "alert, i-frame, etc" only. 
+# in other words it will take "alert, i-frame, etc"" as the driver until is is reverted.
+
+#driver.switch_to.frame(i_frame)
+
+#________________________________________________________________________________________
+
+# use this to go back to default driver
+
+#driver.switch_to.default_content()
+
+
--- wordpress.py
+++ wordpress.py
@@ -1,6 +1,71 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.keys import Keys
 
-url = "https://wpsandbox.net"
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 
-driver = webdriver.Edge()
+# set options to make browser not to close
+options = Options()
+options.add_experimental_option("detach", True)
+
+url = "https://s-qkqgtyg4wbc3l.eu1.wpsandbox.org/wp-admin/"
+
+# add "option=options" below
+driver = webdriver.Edge(options=options)
+driver.maximize_window()
+
+driver.get(url)
+
+access_code = driver.find_element(By.CLASS_NAME, value="form-control")
+access_code.send_keys(607)
+access_code.send_keys(Keys.ENTER)
+
+wordpress_username = driver.find_element(By.XPATH, "//input[@type='text']")
+wordpress_username.send_keys("adminuser391651")
+wordpress_password = driver.find_element(By.ID, value="user_pass")
+wordpress_password.send_keys("cvBbKdPCKB")
+
+wordpress_password.send_keys(Keys.ENTER)
+
+driver.find_element(By.XPATH, "//a[@href='https://s-qkqgtyg4wbc3l.eu1.wpsandbox.org/wp-admin/post-new.php']").click()
+
+add_title_cover = driver.find_element(By.CLASS_NAME, value="block-editor-iframe__container")
+
+i_frame = add_title_cover.find_element(By.XPATH, ".//iframe[@name='editor-canvas']")
+
+# use this below to switch the driver to other sections like; alert, i-frame etc. this means that if "driver." is used, it will be searching for elements within "alert, i-frame, etc" only. 
+# in other words it will take "alert, i-frame, etc"" as the driver until is is reverted.
+
+driver.switch_to.frame(i_frame)
+
+body = driver.find_element(By.XPATH, ".//div[@class='editor-visual-editor__post-title-wrapper edit-post-visual-editor__post-title-wrapper']")
+
+add_title = WebDriverWait(body,20).until(ec.presence_of_element_located((By.TAG_NAME, "h1")))                                                                                  
+add_title.send_keys("We Are The Champions")
+
+content = driver.find_element(By.TAG_NAME, "p")
+content.send_keys("one for the road")
+
+
+# use this to go back to default driver
+driver.switch_to.default_content()
+
+
+publish = driver.find_element(By.XPATH, "//button[@class='components-button editor-post-publish-panel__toggle editor-post-publish-button__button is-primary is-compact']")
+publish.click()
+
+publish_2 = driver.find_element(By.XPATH, "//button[@class='components-button editor-post-publish-button editor-post-publish-button__button is-primary is-compact']")
+publish_2.click()
+
+driver.find_element(By.XPATH, "//a[@href='https://s-qkqgtyg4wbc3l.eu1.wpsandbox.org/we-are-the-champions-2/']").click()
+
+# view_post_container = driver.find_element(By.XPATH, "//div[@class='editor-post-publish-panel__content']")
+
+
+
+# view_post = WebDriverWait(driver,20).until(ec.presence_of_element_located((By.XPATH, "//a[@href='https://s-qkqgtyg4wbc3l.eu1.wpsandbox.org/we-are-the-champions-2/]")))
+# view_post.click()
+
+

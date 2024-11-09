ed7da527c78900b4eb1108abfed30140ea56e37e
jaybuck08 <felix.jr.njoku@gmail.com>
2024-11-09 11:56:47 +0000

    09/11/24

--- wordpress.py
+++ wordpress.py
@@ -43,7 +43,7 @@ add_title_cover = driver.find_element(By.CLASS_NAME, value="block-editor-iframe_
 i_frame = add_title_cover.find_element(By.XPATH, ".//iframe[@name='editor-canvas']")
 
 # use this below to switch the driver to other sections like; alert, i-frame etc. this means that if "driver." is used, it will be searching for elements within "alert, i-frame, etc" only. 
-# in other words it will take "alert, i-frame, etc"" as the driver until is is reverted.
+# in other words it will take "alert, i-frame, etc"" as the driver until is is reverted..
 
 driver.switch_to.frame(i_frame)
 

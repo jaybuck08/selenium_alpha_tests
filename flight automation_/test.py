9bfefbe8abc531e9f6c67ed500512e9ea69c144a
jaybuck08 <felix.jr.njoku@gmail.com>
2025-05-10 20:02:30 +0100

    update

--- flight automation_/test.py
+++ flight automation_/test.py
@@ -24,8 +24,8 @@
 #__________
 
 #  use the below script if you need to read another file into your current file
-with open ("write article or file name here.txt") as file:
-    result = file.read()
+# with open ("write article or file name here.txt") as file:
+#     result = file.read()
 
 
 # _________________________Variables_____________________________________
@@ -62,15 +62,85 @@ football_name = "michael angelo"
 # print(football_name[5::-2])
 
 #1
-NAME = "anabella"
+# NAME = "anabella"
 
-if NAME.startswith("a"):
-    NAME = NAME.upper()
+# if NAME.startswith("a"):
+#     NAME = NAME.upper()
 
-print(NAME)
+# print(NAME)
 
-#2
-if NAME[0].islower():
-    print("first letter is lowercase")
-else:
-    print("first lteer is not lowercase")
+# #2
+# if NAME[0].islower():
+#     print("first letter is lowercase")
+# else:
+#     print("first lteer is not lowercase")
+
+
+# __________using print, sep, end ________________________________________________
+
+# message = "Hello World"
+# name = "John"
+# print(message, "How are you doing", name, sep=",")
+
+# "\n" ---new line character
+# "end=*"   --- this ensures the end of the word ends with an "*". if nothing is added, it ends with a space.
+# sep="," : this is used to seperate each entry of the output with a "," or whatever is put in there
+
+# age = int(input("Enter your age: "))
+# print(type(age))
+
+# ____exercise_____________
+
+# get an input of username and check if name is fully aplphabet
+# else print name is invalid
+
+# username = input("name: ")
+
+# if username.isalpha(): 
+#     print(username,"name is valid",sep="***")
+
+# else:
+#     print("name is invalid")
+
+
+# ____exercise______________
+
+#1 get an input of a combination of names with valid and invalid names
+# namebasket = input("names: ")
+
+# #2 split names to valid & invalid. This splits the names from the commas. if its a "." between the names, it will split from that
+# names = namebasket.split(",")
+
+# #3 create an empty list 
+# valid_names=[]
+
+# #4 check for valid names and put in new list
+# for name in names:
+#     if name.isalpha():
+#         valid_names.append(name)
+    
+# #5 print the valid names
+# print(".".join(valid_names))
+# print("here are the alphabetical names:",valid_names)
+
+
+#____exercise_________________
+
+# write a program that loops through names and extracts the vowels
+# put in a list and print out.
+
+# 1 get an input of names
+names = input("firstname: ")
+
+#2 define vowels
+vowels = "aeiou"
+
+#3 create an empty list to store the vowels extracted
+vowel_list = []
+
+# # 4 Loop through each name and print
+for vowel_letters in names:
+    if vowel_letters in vowels:
+      vowel_list.append(vowel_letters)
+
+print(vowel_list)

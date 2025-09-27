30fae6ceb2d5696ce9290c867282f1d001e60f09
jaybuck08 <felix.jr.njoku@gmail.com>
2025-09-27 13:22:45 +0100

    update

--- others/boxing.py
+++ others/boxing.py
@@ -30,19 +30,32 @@ driver.get(url)
 fighters_container = driver.find_element(by=By.XPATH, value='//div[@class="mt-ringtvGapMobile grid grid-cols-12 gap-ringtvGapMobile md:gap-ringtvGapIpad xl:gap-ringtvGapIpad"]')
 fighters = fighters_container.find_elements(by=By.XPATH, value='.//div[@class="col-span-12 flex flex-col justify-start gap-ringtvGapMobile overflow-hidden rounded-[2rem] bg-[#FFFBF1] p-4 sm:col-span-6 lg:col-span-4 xl:col-span-3 3xl:col-span-2"]')
 
+for index in range(len(fighters)):
 
-for fighter in fighters:
-     
-    details = fighter.click()
+    fighter = driver.find_elements (by=By.XPATH, value='.//div[@class="col-span-12 flex flex-col justify-start gap-ringtvGapMobile overflow-hidden rounded-[2rem] bg-[#FFFBF1] p-4 sm:col-span-6 lg:col-span-4 xl:col-span-3 3xl:col-span-2"]')[index]
+
+    fighter.click()
 
     WebDriverWait(driver,10).until(ec.presence_of_all_elements_located((By.XPATH, '//button[@class="w-full rounded-[1.125rem] py-4 font-bold uppercase ar:font-arabicFont bg-ringtv-red text-white "]')))
-    
-    driver.back()
-    time.sleep(5)
-    
-    # (By.TAG_NAME, "a" ).get_attribute("href")
-    # print(details)
+    detail_container = driver.find_element(by=By.XPATH, value = '//div[@class="x:gap-[40px] flex flex-col gap-[20px] p-[20px] xl:p-[40px]"]')
 
+    fighter_name = detail_container.find_element(by=By.XPATH, value=".//div[@class='text-xheadingFont text-ringtv-black']")
+    fighter_details = detail_container.find_elements(by=By.XPATH, value=".//div[@class='flex  flex-col justify-between gap-4 xsm:flex-row lg:justify-normal lg:gap-10 xl:gap-20 2xl:max-w-[70%]']")
+
+    # print(fighter_name.text)
+
+    for details in fighter_details:
+
+        print(details.text)
+
+        nickname = details.find_element(by=By.XPATH, value=".//span[@class='text-nowrap font-roboto ar:font-arabicFont ar:!font-bold']").text
+        # print(nickname)
+
+
+    driver.back()
+    # time.sleep(5)
+    
+   
   
 
         
